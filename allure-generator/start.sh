fef28c951cdc62541da7502eb29cb531409e212c
anjiboddupally <boddupally.anji@gmail.com>
2024-08-19 10:36:36 +0100

    Initial setup of the local test environment

---
+++ allure-generator/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Wait for the playwright-tests service to generate allure-results
+echo "Waiting for allure-results to be populated..."
+while [ ! "$(ls -A /tests/allure-results)" ]; do
+    sleep 5
+done
+
+echo "allure-results are available. Generating report..."
+allure generate /tests/allure-results --clean -o /tests/allure-report
---
+++ test-project/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Print the working directory
+pwd
+
+# List files in the current directory
+ls
+
+# Run Playwright tests
+npm run test:docker:chrome
