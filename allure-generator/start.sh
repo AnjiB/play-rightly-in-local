b230e7059672e2082d6d2cbf63fa6f5955fe6978
anjiboddupally <boddupally.anji@gmail.com>
2024-08-20 17:03:44 +0100

    Fixing sync issues and running tests in headless mode

--- allure-generator/start.sh
+++ allure-generator/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Hard wait -> debug later
+ sleep 30
 # Wait for the playwright-tests service to generate allure-results
 echo "Waiting for allure-results to be populated..."
 while [ ! "$(ls -A /tests/allure-results)" ]; do
--- test-project/entrypoint.sh
+++ test-project/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# Print the working directory
-pwd
-
-# List files in the current directory
-ls
 
 # Run Playwright tests
-npm run test:docker:chrome
+npm run test:chrome
