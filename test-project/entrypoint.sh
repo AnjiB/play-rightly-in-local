aa0b1768c2b8ab7cb2a67df6e33dd1e1869b0a0f
anjiboddupally <boddupally.anji@gmail.com>
2024-08-20 20:26:40 +0100

    Optimised docker compose

--- test-project/entrypoint.sh
+++ test-project/entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 
+# Directory where allure results are stored
+RESULTS_DIR="/tests/allure-results"
+
+DEFAULT_PORT=8089
+PORT=${ALLURE_PORT:-$DEFAULT_PORT}
+
+# Check if the directory exists and delete it
+if [ -d "$RESULTS_DIR" ]; then
+  echo "Deleting existing Allure results directory..."
+  rm -rf "$RESULTS_DIR"
+fi
+
 # Run Playwright tests
 npm run test:chrome
+
+echo "Playwright tests completed."
+
+echo "Generating allure report"
+
+npx allure serve allure-results -p $PORT
