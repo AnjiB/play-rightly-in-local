0a64beb498691bd1d6b6e2b0a5041a4f1d185840
anjiboddupally <boddupally.anji@gmail.com>
2024-09-12 21:23:54 +0100

    Replaced chrome with chromium as chrome is not working on M1 mac arm64 architecture

--- test-project/entrypoint.sh
+++ test-project/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+JAVA_PATH=$(dirname $(dirname $(readlink -f $(which java))))
+export JAVA_HOME=$JAVA_PATH
+export PATH=$JAVA_HOME/bin:$PATH
+
 # Directory where allure results are stored
 RESULTS_DIR="/tests/allure-results"
 
 DEFAULT_PORT=8089
 PORT=${ALLURE_PORT:-$DEFAULT_PORT}
-BROWSER=${BROWSER:-chrome}
+BROWSER=${BROWSER:-chromium}
 
 # Check if the directory exists and delete it
 if [ -d "$RESULTS_DIR" ]; then
